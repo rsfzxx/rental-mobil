0fe7839755db06621a15c386df4dbfc5dfbb5c72
rsfzxx <rismanmuhammadhafidz21@gmail.com>
2024-05-07 23:25:04 +0700

    First Commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <div class="container mt-5">
+        <div class="judul">Dashboard - Admin</div>
+        <div class="judul">"Bengkelin"</div>
+        <div class="text"> Profesional x Reliable x Affordable </div>
+        <div class="row justify-content-center">
+            <div class="col-md-2 text-center">
+                <button class="btn btn-primary btn-lg">
+                    <i class="bi bi-clock"></i>&nbsp; Data Lama
+                </button>
+            </div>
+            <div class="col-md-2 text-center">
+                <a href="{{ route('buatdata.create') }}">
+                    <button class="btn btn-success btn-lg">
+                        <i class="bi bi-plus-circle-fill"></i>&nbsp;  Buat Baru
+                    </button>
+                </a>
+            </div>
+        </div>          
+    </div>
+</x-app-layout>
