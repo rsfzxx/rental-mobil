a363905de964aebdef471e42b09b66761985f2fe
rsfzxx <rismanmuhammadhafidz21@gmail.com>
2024-05-10 04:55:31 +0700

    Finish

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -12,54 +12,63 @@
         <h2 class="mt-5 mb-3">Formulir Service Kendaraan</h2>
         <form action="/print" method="post">
             @csrf
-            <div class="form-group">
-                <label for="no_spk">NO.SPK</label>
-                <input type="text" class="form-control" id="no_spk" name="no_spk">
-            </div>
-            <div class="form-group">
-                <label for="tanggal">TANGGAL</label>
-                <input type="date" class="form-control" id="tanggal" name="tanggal">
-            </div>
-            <div class="form-group">
-                <label for="pemilik">PEMILIK</label>
-                <input type="text" class="form-control" id="pemilik" name="pemilik">
-            </div>
-            <div class="form-group">
-                <label for="no_telp">NO.TELP</label>
-                <input type="tel" class="form-control" id="no_telp" name="no_telp">
-            </div>
-            <div class="form-group">
-                <label for="alamat">ALAMAT</label>
-                <textarea class="form-control" id="alamat" name="alamat"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="jenis_mobil">JENIS MOBIL</label>
-                <input type="text" class="form-control" id="jenis_mobil" name="jenis_mobil">
-            </div>
-            <div class="form-group">
-                <label for="no_polisi">NO.POLISI</label>
-                <input type="text" class="form-control" id="no_polisi" name="no_polisi">
-            </div>
-            <div class="form-group">
-                <label for="km_masuk">KM MASUK</label>
-                <input type="number" class="form-control" id="km_masuk" name="km_masuk">
-            </div>
-            <div class="form-group">
-                <label for="mekanik">MEKANIK</label>
-                <input type="text" class="form-control" id="mekanik" name="mekanik">
-            </div>
-            <div class="form-group">
-                <label for="jam_masuk">JAM MASUK</label>
-                <input type="time" class="form-control" id="jam_masuk" name="jam_masuk">
-            </div>
-            <div class="form-group">
-                <label for="jam_selesai">JAM SELESAI</label>
-                <input type="time" class="form-control" id="jam_selesai" name="jam_selesai">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="no_spk">NO.SPK</label>
+                            <input type="text" class="form-control" id="no_spk" name="no_spk">
+                        </div>
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="tanggal">TANGGAL</label>
+                            <input type="date" class="form-control" id="tanggal" name="tanggal">
+                        </div>
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="pemilik">PEMILIK</label>
+                            <input type="text" class="form-control" id="pemilik" name="pemilik">
+                        </div>
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="no_telp">NO.TELP</label>
+                            <input type="tel" class="form-control" id="no_telp" name="no_telp">
+                        </div>
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="alamat">ALAMAT</label>
+                            <textarea class="form-control" id="alamat" name="alamat"></textarea>
+                        </div>
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="jenis_mobil">JENIS MOBIL</label>
+                            <input type="text" class="form-control" id="jenis_mobil" name="jenis_mobil">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="no_polisi">NO.POLISI</label>
+                            <input type="text" class="form-control" id="no_polisi" name="no_polisi">
+                        </div>
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="km_masuk">KM MASUK</label>
+                            <input type="number" class="form-control" id="km_masuk" name="km_masuk">
+                        </div>
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="mekanik">MEKANIK</label>
+                            <input type="text" class="form-control" id="mekanik" name="mekanik">
+                        </div>
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="jam_masuk">JAM MASUK</label>
+                            <input type="time" class="form-control" id="jam_masuk" name="jam_masuk">
+                        </div>
+                        <div class="form-group col-md-12 mb-3">
+                            <label for="jam_selesai">JAM SELESAI</label>
+                            <input type="time" class="form-control" id="jam_selesai" name="jam_selesai">
+                        </div>
+                    </div>
+                </div>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
-    <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -6,8 +6,40 @@
     <title>Print PDF</title>
     <style>
         body {
-            border: 1px solid #000;
-            padding: 20px;
+            border: 2px solid #000;
+        }
+        .container {
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
+        }
+
+        .row {
+            flex: 1;
+        }
+
+        .row:nth-child(1) {
+            height: 12%;
+        }
+
+        .row:nth-child(2) {
+            height: 5%;
+        }
+
+        .row:nth-child(3) {
+            height: 15%;
+        }
+
+        .row:nth-child(4) {
+            height: 20%;
+        }
+
+        .row:nth-child(5) {
+            height: 11%;
+        }
+
+        .row:nth-child(6) {
+            height: 35%;
         }
 
         .column {
@@ -45,30 +77,229 @@
             margin-top : 7px;
             font-size : 0.35rem;
         }
+
+        .payment-box {
+            width: 10px;
+            height: 10px;
+            display: inline-block;
+            border: 1px solid black;
+            margin-top : 10px;
+            vertical-align: middle;
+        }
+
+        .payment {
+            border-top:1px solid #000;
+            padding: 12px;
+            font-weight:bold;
+            text-align:center;
+            font-size: 0.8rem;
+        }
+
+        .pemeriksaan {
+            border-top:1px solid #000;
+            padding :12px;
+            font-weight:bold;
+            font-size: 0.8rem;
+        }
+
+        .servis {
+            border-top:1px solid #000;
+            padding :12px;
+            font-weight:bold;
+        }
+
+        .item {
+            margin: 3px;
+            margin-top:10px;
+            margin-left:10px;
+            font-size:0.7rem;
+            font-weight: bold;
+            letter-spacing: -0.5px;
+        }
+
+        .tambahan{
+            font-size:0.7rem;
+        }
+        .box{
+            width: 7px;
+            margin-right:1px;
+            height: 7px;
+            display: inline-block;
+            border: 1px solid black;
+            vertical-align: middle;
+            margin-top: 3px;
+        }
+
+        .boxservis {
+            width: 15px;
+            height: 15px;
+            display: inline-block;
+            border: 1px solid black;
+            vertical-align: middle;
+            margin-right: 5px;
+            margin-left: 10px;
+            margin-top:8px;
+        }
+
+        .catatan{
+            border-top:1px solid #000;
+            font-weight:bold;
+        }
+
+        .catatan::after {
+            content: ' ..............................................................................................................................................................................................................';
+            font-size: 10px;
+        }
+        .titik::after {
+            content: '............................................................................................................................................................................................................................................................................';
+            font-size: 10px;
+            font-weight: bold;
+            padding:12px;
+        }
+
+        .borderlast{
+            border-right:1px solid #000;
+        }
+
+        .itemlast{
+            font-weight:bold;
+        }
+
+        .textlast{
+            font-size:10px;
+            padding:5px;
+        }
+
     </style>
 </head>
 <body>
-    <div class="column">
-        <div class="judul">Bengkelin</div>
-        <div class="text">Profesional x Reliable x Affordable</div>
-    </div>
-    <div class="column">
-        <p>NO.SPK: {{ $no_spk }}</p>
-        <p>TANGGAL: {{ $tanggal }}</p>
-        <p>PEMILIK: {{ $pemilik }}</p>
-        <p>NO.TELP: {{ $no_telp }}</p>
-        <p>ALAMAT: {{ $alamat }}</p>
-    </div>
-    <div class="column">
-        <p>JENIS MOBIL: {{ $jenis_mobil }}</p>
-        <p>NO.POLISI: {{ $no_polisi }}</p>
-        <p>KM MASUK: {{ $km_masuk }}</p>
-        <p>MEKANIK: {{ $mekanik }}</p>
-    </div>
-    <div class="column">
-        <p>JAM MASUK: {{ $jam_masuk }}</p>
-        <p>JAM SELESAI: {{ $jam_selesai }}</p>
-        <div class="small">*Di konfirmasi ulang oleh mekanik</div>
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                <div class="judul">Bengkelin</div>
+                <div class="text">Profesional x Reliable x Affordable</div>
+            </div>
+            <div class="column">
+                <p>NO.SPK: {{ $no_spk }}</p>
+                <p>TANGGAL: {{ $tanggal }}</p>
+                <p>PEMILIK: {{ $pemilik }}</p>
+                <p>NO.TELP: {{ $no_telp }}</p>
+                <p>ALAMAT: {{ $alamat }}</p>
+            </div>
+            <div class="column">
+                <p>JENIS MOBIL: {{ $jenis_mobil }}</p>
+                <p>NO.POLISI: {{ $no_polisi }}</p>
+                <p>KM MASUK: {{ $km_masuk }}</p>
+                <p>MEKANIK: {{ $mekanik }}</p>
+            </div>
+            <div class="column">
+                <p>JAM MASUK: {{ $jam_masuk }}</p>
+                <p>JAM SELESAI: {{ $jam_selesai }}</p>
+                <div class="small">*Di konfirmasi ulang oleh mekanik</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="payment">
+                <div class="column" style="text-align:left; font-size:20px; width: 25%;">PEMBAYARAN</div>
+                <div class="column" style="width: 17%;">
+                    <div class="payment-box"></div> CASH
+                </div>
+                <div class="column" style="width: 17%;">
+                    <div class="payment-box"></div> DEBET
+                </div>
+                <div class="column" style="width: 17%;">
+                    <div class="payment-box"></div> CREDIT
+                </div>
+                <div class="column" style="text-align:left; width: 24%;">
+                    <div class="payment-box"></div> LAINNYA..........
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="pemeriksaan">
+                <div class="row" style="margin-bottom:10px; text-align:left; font-size:20px; width: 50%;">PEMERIKSAAN AWAL</div>
+                <div class="column">
+                    <div class="item"><div class="box"></div> OLI MESIN</div>
+                    <div class="item"><div class="box"></div> OLI TRANSMISI</div>
+                    <div class="item"><div class="box"></div> REM</div>
+                    <div class="item"><div class="box"></div> MASTER REM</div>
+                </div>
+                <div class="column">
+                    <div class="item"><div class="box"></div> MINYAK POWER STEERING</div>
+                    <div class="item"><div class="box"></div> RADIATOR</div>
+                    <div class="item"><div class="box"></div> AIR RADIATOR</div>
+                    <div class="item"><div class="box"></div> FAN BELT</div>
+                </div>
+                <div class="column">
+                    <div class="item"><div class="box"></div> SARINGAN UDARA</div>
+                    <div class="item"><div class="box"></div> SARINGAN AC</div>
+                    <div class="item"><div class="box"></div> LAMPU LAMPU</div>
+                    <div class="item"><div class="box"></div> WIPER</div>
+                </div>
+                <div class="column">
+                    <div class="item"><div class="box"></div> AIR WIPER</div>
+                    <div class="item"><div class="box"></div> AKI</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="servis">
+                <div class="row" style="margin-bottom:10px; text-align:left; font-size:20px; width: 50%;">SERVIS TAMBAHAN</div>
+                <div class="column" style="width: 50%;">
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                </div>
+                <div class="column" style="width: 50%;">
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                    <div class="tambahan"><div class="boxservis"></div>.......................................................................................</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="catatan" style="padding-top:10px; font-size:20px; width:100%;"><span style="padding-left:12px;">CATATAN LAIN</span></div>
+            <div class="titik" style="margin-top:8px"></div>
+            <div class="titik" style="margin-top:10px"></div>
+            <div class="titik" style="margin-top:10px"></div>
+        </div>
+        <div class="row">
+            <div class="column" style="width: 50%; border-top: 1px solid black;">
+                <div class="itemlast" style="height: 50%;">
+                    <div class="textlast" style="padding:5px; font-size:15px;"> INSPEKSI MOBIL</div>
+                </div>
+                <div class="itemlast" style="height: 50%;">
+
+                </div>
+            </div>
+            <div class="column" style="width: 50%; border-left: 1px solid black; border-top: 1px solid black;">
+                <div class="itemlast" style="height: 40%;">
+                    <div class="textlast" style="padding:5px; font-size:15px;"> HISTORY/RIWAYAT SERVIS</div>
+                    <div class="textlast">TANGGAL TERAKHIR SERVIS :</div>
+                    <div class="textlast">KM :</div>
+                    <div class="textlast">PEKERJAAN :</div>
+                </div>
+                <div class="itemlast" style="border-top:1px solid #000; height: 20%;">
+                    <div class="textlast" style="padding:5px; font-size:15px;"> ESTIMASI BIAYA</div>
+                    <div class="textlast">RP. ........................................................................................................................</div>
+                    <div class="small" style="padding-left:5px;">*Di konfirmasi oleh sa/mekanik</div>
+                </div>
+                <div class="itemlast" style="border-top: 1px solid #000; height: 40%;">
+                    <div class="textlast" style="padding: 5px; font-size: 15px;">TANDA TANGAN
+                        <div class="textlast" style="font-size: 15px; display: flex; justify-content: space-between;">
+                            <span class="textlast" style="font-size: 15px; padding-right:100px;">SA/MEKANIK</span>
+                            <span class="textlast" style="font-size: 15px;">CUSTOMER</span>
+                        </div>         
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
